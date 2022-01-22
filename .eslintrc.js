b3c0a5c0dd0baed0dca6e4636011862e61bca39a
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-01-22 00:38:06 -0300

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
