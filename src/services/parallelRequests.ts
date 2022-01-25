b479e39498b31f920a50763ea752920f8aefcd6a
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-01-24 22:30:37 -0300

    fix: ensures data from all requests has been successfully collected

--- src/services/parallelRequests.ts
+++ src/services/parallelRequests.ts
@@ -1,28 +1,30 @@
 import axiosParallel, { AxiosParallelInput, AxiosParallelResponse } from 'axios-parallel'
 
-export async function parallelRequests (requests: AxiosParallelInput, allSuccededResponses: AxiosParallelResponse[]): Promise<AxiosParallelResponse[]> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function parallelRequests (requests: AxiosParallelInput | any, allSuccededResponses: AxiosParallelResponse[]): Promise<AxiosParallelResponse[]> {
   try {
     const MAX_PARALLEL_REQUEST_PER_CPU = 100
     const responses: AxiosParallelResponse[] = await axiosParallel(requests, MAX_PARALLEL_REQUEST_PER_CPU)
     const succededResponses: AxiosParallelResponse[] = []
-    const failedRequests: AxiosParallelInput = []
 
     responses.forEach(function (response) {
       if (response.details.statusCode === 200) {
         succededResponses.push(response)
-      } else {
-        failedRequests.push({
-          method: 'GET',
-          url: response.details.responseUrl
-        })
       }
     })
 
-    if (failedRequests.length === 0) {
+    succededResponses.forEach((response) => {
+      requests.splice(requests.indexOf({
+        method: 'GET',
+        url: response.details.responseUrl
+      }), 1)
+    })
+
+    if (requests.length === 0) {
       return [...succededResponses, ...allSuccededResponses]
     }
 
-    return parallelRequests(failedRequests, [...succededResponses, ...allSuccededResponses])
+    return parallelRequests(requests, [...succededResponses, ...allSuccededResponses])
   } catch (error) {
     throw new Error(error)
   }
